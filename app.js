096d6ee45088e7bb290acad2cb50122f3a4403ba
Eduardo Colina <eacolina@edu.uwaterloo.ca>
2019-01-30 19:21:44 -0500

    Refactored

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ var lastEventBlock = 1909278// always update this
 var tokenHolders = {} // this dict will hold all the BURN token owners and their balance
 const xdaiEndpoint = process.env.RPC_ENDPOINT
 var account
-var dropAmount 
+var dropAmount
+var doneFirstRun = false
+var log = true
 
 // Returns a wallet provider pointing to the xDai RPC
 function xDaiproviderSetup(){
@@ -29,51 +31,32 @@ async function initXDAIService() {
     dropAmount = utils.toWei('0.02') // this is har coded to 2 cents for now
     account = (await web3.eth.getAccounts())[0] // TODO setup with proper account for DAI pool
     xDaiEmitterContract = new eth.Contract(BURN_ABI, process.env.TOKEN_ADDRESS, {from: account})
-    getInitialOwners() //
-    setTimeout(logOwners,40000) // pwait 40 secs to get all the owners
+    console.log("Getting initial owners")
+    getTransferEvents() //
 }
 
-function logOwners(){
-    console.log(tokenHolders) // log the results
-    console.log("Starting to poll")
-    getTransferEvents() // start polling
-}
 
-async function getInitialOwners(){
+async function getTransferEvents(){
     xDaiEmitterContract.getPastEvents("Transfer",{fromBlock:lastEventBlock, toBlock:'latest'},TransferCBInitial)
+    if(doneFirstRun){
+        if(log){
+            console.log(tokenHolders)
+            console.log("Starting to poll")
+            log = false
+        }
+        setTimeout(getTransferEvents,150)
+    } else {
+        setTimeout(getTransferEvents,40000)
+    }
 }
-// this will poll every 150ms for new Transfer events
-function getTransferEvents(){
-    // Get all the past "FundsSent" events
-    xDaiEmitterContract.getPastEvents("Transfer",{fromBlock:lastEventBlock, toBlock:'latest'},TransferCB)
-    setTimeout(getTransferEvents,150) // poll every 150ms 
-}
-
 
 
 async function TransferCBInitial(err, res){
     if(err){
         console.log("There was an error with the RPC endpoint")
     } else {
-        var latest_block = (res[res.length - 1]).blockNumber
+        var latest_block = (res[res.length - 1]).blockNumber // get the latest event block number
         if(latest_block > lastEventBlock) {
-            lastEventBlock = latest_block // so that next time we only get events starting from that block
-            for(var i = 0; i < res.length; i++){
-                var address = res[i].returnValues.to
-                if(!(address in tokenHolders)){
-                    tokenHolders[address] = await eth.getBalance(address)
-                }
-            }
-        }
-    }
-}
-async function TransferCB(err, res){
-    if(err){
-        console.log("There was an error with the RPC endpoint")
-    } else {
-        var latest_block = (res[res.length - 1]).blockNumber
-        if(latest_block > lastEventBlock) {
-            console.log("Heard something")
             lastEventBlock = latest_block // so that next time we only get events starting from that block
             for(var i = 0; i < res.length; i++){
                 var address = res[i].returnValues.to
@@ -82,20 +65,24 @@ async function TransferCB(err, res){
                     tokenHolders[address] = balance
                     if(balance == 0){
                         supplyGas(address)
+                        tokenHolders[address] = utils.toWei('0.02')
                     }
                 }
             }
         }
+        doneFirstRun = true
     }
 }
 
-
 async function supplyGas(dest_account){
-    eth.sendTransaction({from:account, to:dest_account, value: dropAmount, gas:210000, gasPrice:20000000000}).then((res) => {
-        console.log("Just sent", dropAmount, "DAI to account", dest_account)
-        console.log('TxHash:',res.transactionHash)
-    }).catch((err) => {
-        console.error("There was an error sending the funds: ", err,"\n")
+    var nonce = await eth.getTransactionCount(account)
+    eth.sendTransaction({from:account, to:dest_account, value: dropAmount, gas:210000, gasPrice:20000000000,nonce:nonce},(err,res) =>{
+        if(err != undefined){
+            console.log(err)
+        } else {
+            console.log("Just sent", dropAmount, "DAI to account", dest_account)
+            console.log('TxHash:',res)
+        }
     })
 }
 
